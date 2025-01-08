29a9599025deb3fd4c93debbfbc59ba4d1de75ef
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2025-01-09 01:19:09 +0530

    minor changes

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -11,7 +11,7 @@ router.get('/all', userController.getAllUsers);
 router.post('/login', userController.login);
 router.post('/protected', userController.protected);
 router.get('/getUser/:username', userController.getUser);
-router.post('/updateUser/:userId', userController.updateUser);
+router.patch('/updateUser/:userId', userController.updateUser);
 router.post('/userCheck', userController.userCheck);
 router.post('/updatePassword', userController.updatePassword);
  
